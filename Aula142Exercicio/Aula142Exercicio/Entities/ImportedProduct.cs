2fe0496da5e80429e85702db0cb94430b2de40c7
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-02 15:13:47 -0300

    Create ImportedProduct.cs
    Cria subclasse - sobreescreve função - cria função adicional

---
+++ Aula142Exercicio/Aula142Exercicio/Entities/ImportedProduct.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula142Exercicio.Entities
+{
+    public class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return CustomsFee + Price;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} ${TotalPrice().ToString("F2")} (Custom fee: ${CustomsFee}";
+        }
+    }
+}
