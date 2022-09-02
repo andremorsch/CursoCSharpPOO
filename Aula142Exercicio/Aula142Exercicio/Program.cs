99bc4a91f7b61f22260d7baad1b615fd4377fb6d
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-02 16:03:47 -0300

    Create Program
    Personaliza programa para utilizar as classes conforme exercicio

--- Aula142Exercicio/Aula142Exercicio/Program.cs
+++ Aula142Exercicio/Aula142Exercicio/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aula142Exercicio.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace Aula142Exercicio
 {
@@ -6,7 +8,46 @@ namespace Aula142Exercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> listProducts = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int productsQuantity = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < productsQuantity; i++)
+            {
+                Console.WriteLine($"Product #{i+1} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char cui = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                
+                if (cui == 'c')
+                {
+                    listProducts.Add(new Product(name, price));
+                }
+                else if (cui == 'u')
+                {
+                    Console.Write("Manufacturate date (DD/MM/YYYY): ");
+                    DateTime manufacturateDate = DateTime.Parse(Console.ReadLine());
+
+                    listProducts.Add(new UsedProduct(name, price, manufacturateDate));
+                }
+                else if (cui == 'i')
+                {
+                    Console.Write("Custom fee: ");
+                    double fee = double.Parse(Console.ReadLine());
+
+                    listProducts.Add(new ImportedProduct(name, price, fee));
+                }
+            }
+
+            Console.WriteLine("\nPRICE TAGS:");
+            foreach (Product product in listProducts)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
         }
     }
 }
