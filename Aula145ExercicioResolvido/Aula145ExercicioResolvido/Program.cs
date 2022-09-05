cb463fc580368dd1a47c14ccf6ee0fedad361e69
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-05 15:08:59 -0300

    Create Program
    Cria o programa para utilizar as classes conforme enunciado do exercicio

--- Aula145ExercicioResolvido/Aula145ExercicioResolvido/Program.cs
+++ Aula145ExercicioResolvido/Aula145ExercicioResolvido/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Aula145ExercicioResolvido.Entities;
+using Aula145ExercicioResolvido.Entities.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace Aula145ExercicioResolvido
 {
@@ -6,7 +9,39 @@ namespace Aula145ExercicioResolvido
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Shape> list = new List<Shape>(); 
+
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nShape #{i+1} data:");
+                Console.Write("Rectangle or circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+                if (ch == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine());
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine());
+                    list.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine());
+                    list.Add(new Circle(radius, color));
+                }
+            }
+
+            Console.WriteLine("\n\nShape Areas:");
+            foreach (Shape shape in list)
+            {
+                Console.WriteLine($"{shape.Area():F2}");
+            }
         }
     }
 }
