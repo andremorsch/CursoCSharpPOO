b8592bf7af0ea5295c38b1443390619269c6e357
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-05 15:08:26 -0300

    Create Circle.cs
    Cria subclasse Circle e subscreve função area

---
+++ Aula145ExercicioResolvido/Aula145ExercicioResolvido/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿using Aula145ExercicioResolvido.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula145ExercicioResolvido.Entities
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
