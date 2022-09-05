1b7e4d7195e8a53653336aaa275cb34ea1427e6d
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-05 15:08:05 -0300

    Create Rectangle.cs
    Cria Subclasse Rectangle e sobreescreve função Area

---
+++ Aula145ExercicioResolvido/Aula145ExercicioResolvido/Entities/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using Aula145ExercicioResolvido.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula145ExercicioResolvido.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
