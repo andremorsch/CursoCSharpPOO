841064bf961251b3d6f22d80ec6e0d043106da4a
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-05 11:48:16 -0300

    Update SavingsAccount.cs

--- Aula135Heranca/Aula135Heranca/Entities/SavingsAccount.cs
+++ Aula135Heranca/Aula135Heranca/Entities/SavingsAccount.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Aula135Heranca.Entities
 {
-    public class SavingsAccount : Account
+    class SavingsAccount : Account
     {
         public double InterestRate { get; set; }
 
