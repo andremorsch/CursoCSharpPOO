70c9a1ecc7f5cdca9b63a1d9a95bf0914f5b47f6
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-06 10:02:24 -0300

    Inicia exercicio final do modulo

---
+++ Aula146ExercicioProposto/Aula146ExercicioProposto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula146ExercicioProposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
