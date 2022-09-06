82434ea8efb7da9f3afec484ad7fe3a81cfeb073
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-06 16:16:07 -0300

    Update Program.cs
    adiciona algumas exceções

--- Aula150Excecoes/Aula150Excecoes/Program.cs
+++ Aula150Excecoes/Aula150Excecoes/Program.cs
@@ -6,7 +6,22 @@ namespace Aula150Excecoes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine()); // Dividir por 0 para dar o erro
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Division by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error! {e.Message}");
+            }
         }
     }
 }
