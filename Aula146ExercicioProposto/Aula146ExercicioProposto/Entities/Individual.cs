0b83b436c61518eb7d81085708aa13c87cb7b43f
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-06 10:09:56 -0300

    Create Individual.cs
    subclasse + construtor

---
+++ Aula146ExercicioProposto/Aula146ExercicioProposto/Entities/Individual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula146ExercicioProposto.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+    }
+}
