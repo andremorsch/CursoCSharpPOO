c1460197873d3b0abb3cb544f6a4f57c49823ca7
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-06 10:09:46 -0300

    Create Company.cs
    subclasse + construtor

---
+++ Aula146ExercicioProposto/Aula146ExercicioProposto/Entities/Company.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula146ExercicioProposto.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company(string name, double anualIncome, int numberOfEmployees) : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+    }
+}
