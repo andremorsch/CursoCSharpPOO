81939a930583542feb4166c8411a334473a74eb7
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-06 11:28:08 -0300

    Update Program.cs
    Adiciona exercicio conforme enunciado

--- Aula146ExercicioProposto/Aula146ExercicioProposto/Program.cs
+++ Aula146ExercicioProposto/Aula146ExercicioProposto/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aula146ExercicioProposto.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace Aula146ExercicioProposto
 {
@@ -6,7 +8,45 @@ namespace Aula146ExercicioProposto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i + 1} data:");
+                Console.Write("Individual or company (i/c)? ");
+                char ic = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine());
+                if (ic == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine());
+
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else if (ic == 'c')
+                {
+                    Console.Write("Number of employees: ");
+                    int qntEmployees = int.Parse(Console.ReadLine());
+
+                    list.Add(new Company(name, anualIncome, qntEmployees));
+                }
+            }
+
+            Console.WriteLine("\n\nTAXES PAID:");
+            double sumTax = 0;
+            foreach (TaxPayer taxPayer in list)
+            {
+                Console.WriteLine($"{taxPayer.Name}: ${taxPayer.Tax():F2}");
+                sumTax += taxPayer.Tax();
+            }
+
+            Console.WriteLine($"\nTotal Taxes: ${sumTax:F2}");
         }
     }
 }
