0b1af0c2459e6da97887ad4f09701107c3637d67
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-08 11:15:22 -0300

    Create DomainException.cs
    Cria a domainException para erros personalizados

---
+++ Aula152ExercicioResolvido/Aula152ExercicioResolvido/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula152ExercicioResolvido.Entities.Exceptions
+{
+    internal class DomainException
+    {
+    }
+}
