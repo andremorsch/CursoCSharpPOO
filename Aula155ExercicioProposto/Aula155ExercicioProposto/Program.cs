58594198e62c2f4e550d1e8d1c015721b46c7a6e
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-08 11:26:00 -0300

    Inicia desafio proposto

---
+++ Aula155ExercicioProposto/Aula155ExercicioProposto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula155ExercicioProposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
