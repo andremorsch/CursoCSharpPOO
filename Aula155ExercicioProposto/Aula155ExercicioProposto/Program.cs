86f159519937419a8c15d25c574a2cd022e92e4c
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-09 09:28:38 -0300

    Update Program.cs
    cria o programa para testar as exceções

--- Aula155ExercicioProposto/Aula155ExercicioProposto/Program.cs
+++ Aula155ExercicioProposto/Aula155ExercicioProposto/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Aula155ExercicioProposto.Entities;
+using Aula155ExercicioProposto.Entities.Exceptions;
+using System;
 
 namespace Aula155ExercicioProposto
 {
@@ -6,7 +8,30 @@ namespace Aula155ExercicioProposto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int numberAcc = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double initialBalance = double.Parse(Console.ReadLine());
+            Console.Write("Withdraw Limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Account account = new Account(numberAcc, holder, initialBalance, withdrawLimit);
+
+                Console.Write("\nEnter the amount for Withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                account.Withdraw(withdraw);
+
+                Console.WriteLine($"New balance: ${account.Balance}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Withdraw error: {e.Message}");
+            }
         }
     }
 }
