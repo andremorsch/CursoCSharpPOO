6e8619d61c51d32a8a760252f6529fc839635534
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-28 13:38:35 -0300

    Aprendizado de using block

---
+++ Aula197UsingBlock/Aula197UsingBlock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Aula197UsingBlock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\André Morsch\Documents\Extras\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
