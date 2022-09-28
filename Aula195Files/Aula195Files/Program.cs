15157122b15d6f0d298376fe30be50d39acff94b
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-28 11:19:17 -0300

    Inicia seção: Trabalhando com arquivos

---
+++ Aula195Files/Aula195Files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Aula195Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\Users\André Morsch\Documents\Extras\file1.txt";
+            string targetPath = @"c:\Users\André Morsch\Documents\Extras\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
