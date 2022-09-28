6f25ca4e77a5c2a1a884c90adf5f9816e64a215e
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-28 16:27:15 -0300

    Aprendendo a ultilizar o StreamWriter

---
+++ Aula198StreamWriter/Aula198StreamWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Aula198StreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\André Morsch\Documents\Extras\file1.txt";
+            string targetPath = @"C:\Users\André Morsch\Documents\Extras\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
