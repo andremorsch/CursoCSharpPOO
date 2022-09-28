c0fd66679b6b9a96c93c34a1e16c02af864e3cda
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-09-28 11:44:12 -0300

    Inicia uso de FileStream e StreamReader

---
+++ Aula196FileStream_FileReader/Aula196FileStream_FileReader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Aula196FileStream_FileReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\André Morsch\Documents\Extras\file1.txt";
+            //FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                //fs = new FileStream(path, FileMode.Open);
+                //sr = new StreamReader(fs);
+                sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error acurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(sr != null) sr.Close();
+                //if (fs != null) fs.Close();
+            }
+        }
+    }
+}
