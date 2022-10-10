0193dcf460c00fc7956cc1eeecce228cdaf58d5b
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-10-10 17:17:30 -0300

    Cria programa com o requisitado pelo desafio

---
+++ Aula208ExercicioInterfaces/Aula208ExercicioInterfaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using Aula208ExercicioInterfaces.Entities;
+using Aula208ExercicioInterfaces.Services;
+using System;
+using System.Globalization;
+
+namespace Aula208ExercicioInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
