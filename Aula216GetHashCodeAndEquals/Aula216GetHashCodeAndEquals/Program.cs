35c21d20d4603b5a99cdddc157d1a7b502d04ca1
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-10-12 22:55:41 -0300

    Aula 216
    Teste métodos comparativos com o Equal e GetHashCode - GetHashCode tem uma performance melhor

---
+++ Aula216GetHashCodeAndEquals/Aula216GetHashCodeAndEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aula216GetHashCodeAndEquals.Entities;
+using System;
+
+namespace Aula216GetHashCodeAndEquals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
