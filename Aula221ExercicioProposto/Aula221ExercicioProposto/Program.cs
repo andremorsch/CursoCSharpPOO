795cf4f5cfcbd78b079eb6e189ce3145d8545c6b
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-10-13 00:04:30 -0300

    Exercício proposto
    Utiliza o HashSet + UnionWith, para juntar os alunos de todos os cursos, sendo alunos únicos

---
+++ Aula221ExercicioProposto/Aula221ExercicioProposto/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula221ExercicioProposto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> a = new HashSet<int>();
+            HashSet<int> b = new HashSet<int>();
+            HashSet<int> c = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the student code: ");
+                int x = int.Parse(Console.ReadLine());
+                a.Add(x);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the student code: ");
+                int x = int.Parse(Console.ReadLine());
+                b.Add(x);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the student code: ");
+                int x = int.Parse(Console.ReadLine());
+                c.Add(x);
+            }
+
+            HashSet<int> total = new HashSet<int>(a);
+            total.UnionWith(b);
+            total.UnionWith(c);
+            Console.WriteLine($"\nTotal students: {total.Count}");
+        }
+    }
+}
