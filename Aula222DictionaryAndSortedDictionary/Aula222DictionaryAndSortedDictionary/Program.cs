e6aac7973a435bd88b0d49bf48ca31fbeee6b4e5
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-10-13 00:42:40 -0300

    Usando Dictionary

---
+++ Aula222DictionaryAndSortedDictionary/Aula222DictionaryAndSortedDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula222DictionaryAndSortedDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99711122";
+            cookies["phone"] = "99999999";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+            Console.WriteLine(cookies.ContainsKey("email"));
+
+            Console.WriteLine($"Size: {cookies.Count}");
+            Console.WriteLine("ALL COOKIES:");
+
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+    }
+}
