7f561a7fa6e93a59fe0d255a8c1187771923832d
André Morsch <59461612+andremorsch@users.noreply.github.com>
2022-10-25 13:55:31 -0300

    Exercício resolvido

---
+++ Aula241ExercicioResolvido/Aula241ExercicioResolvido/Program.cs
@@ -0,0 +1,40 @@
+﻿using Aula241ExercicioResolvido.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Aula241ExercicioResolvido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter full file path: ");
+            string path = @"C:\Pessoal2\Aula241ExercicioResolvido\in.txt";
+
+            List<Product> list = new List<Product>();
+            
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            double avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine($"Average price = {avg.ToString("F2")}");
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
